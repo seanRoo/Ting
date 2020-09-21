cce917b7423b583c062a16f344953604f0004712
Sean Rooney <33811391+seanRoo@users.noreply.github.com>
2020-09-21 23:16:23 +0100

    first part of marked dates and change to db

--- src/components/MyCalendar/MyCalendar.js
+++ src/components/MyCalendar/MyCalendar.js
@@ -1,72 +1,105 @@
 import React, {useEffect, useState} from 'react';
 import {Calendar} from 'react-native-calendars';
+import auth from '@react-native-firebase/auth';
+import {DB} from '../../config';
+import {isEmpty} from 'lodash';
+import {Text, View} from 'react-native';
+import Loading from '../Loading';
+import {Center} from '../Center';
 
-const testDates = [
-  {
-    checkedIn: false,
-    date: '2020-09-20',
-  },
-  {
-    checkedIn: true,
-    date: '2020-09-21',
-  },
-  {
-    checkedIn: true,
-    date: '2020-09-22',
-  },
-];
-const handleMarkedDates = () => {
+const handleMarkedDates = (data = {}) => {
   let markedDates = [];
-  markedDates = testDates.reduce((acc, {date, checkedIn}) => {
-    if (checkedIn) {
-      acc[date] = {
-        customStyles: {
-          container: {
-            backgroundColor: 'green',
-          },
-          text: {
-            color: 'white',
-            fontWeight: 'bold',
-          },
+  Object.keys(data).map((key) => {
+    markedDates[key] = {
+      customStyles: {
+        container: {
+          backgroundColor: 'green',
         },
-      };
-    } else {
-      acc[date] = {
-        customStyles: {
-          container: {
-            backgroundColor: 'red',
-          },
-          text: {
-            color: 'white',
-            fontWeight: 'bold',
-          },
+        text: {
+          color: 'white',
+          fontWeight: 'bold',
         },
-      };
-    }
-    return acc;
-  }, {});
+      },
+    };
+  });
   return markedDates;
 };
 
 const MyCalendar = ({navigation}) => {
+  const currentUser = auth().currentUser.uid;
   const [markedDates, setMarkedDates] = useState();
-  useEffect(() => {
-    if (!markedDates) {
-      setMarkedDates(handleMarkedDates());
+  const [currentCalendarDate, setCurrentCalendarDate] = useState(new Date());
+  const [checkIns, setCheckIns] = useState();
+  const [monthYearString, setMonthYearString] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const getCheckIns = (userId, monthYearValue) => {
+    try {
+      DB.ref(`/checkIns/${userId}/${monthYearValue}`).on(
+        'value',
+        (querySnapshot) => {
+          let data = querySnapshot.val() ? querySnapshot.val() : {};
+          setCheckIns(data);
+        },
+      );
+    } catch (error) {
+      console.log(error);
     }
-  }, [markedDates]);
+  };
+
+  const handleMonthUpdate = (date) => {
+    const updatedDate = new Date(date);
+    setCurrentCalendarDate(updatedDate);
+    setMonthYearString(
+      `${updatedDate.getFullYear()}-${updatedDate.getMonth() + 1}`,
+    );
+    getCheckIns(currentUser, monthYearString);
+  };
+
+  useEffect(() => {
+    if (!monthYearString) {
+      setMonthYearString(
+        `${currentCalendarDate.getFullYear()}-${
+          currentCalendarDate.getMonth() + 1
+        }`,
+      );
+    }
+    if (!checkIns && monthYearString) {
+      getCheckIns(currentUser, monthYearString);
+    }
+    if (!markedDates && checkIns) {
+      setMarkedDates(handleMarkedDates(checkIns));
+    }
+    if (!isEmpty(checkIns) && markedDates) {
+      setLoading(false);
+    }
+  }, [markedDates, currentCalendarDate, monthYearString, checkIns]);
   return (
-    <Calendar
-      onDayPress={(day) =>
-        navigation.navigate('Check In', {
-          date: day,
-        })
-      }
-      hideArrows={false}
-      hideExtraDays={true}
-      markedDates={markedDates}
-      markingType={'custom'}
-    />
+    <View>
+      {checkIns && markedDates && (
+        <Calendar
+          onDayPress={(day) =>
+            navigation.navigate('Check In', {
+              date: day,
+              monthYearString,
+            })
+          }
+          hideArrows={false}
+          hideExtraDays={true}
+          markedDates={markedDates}
+          markingType={'custom'}
+          current={currentCalendarDate}
+          onMonthChange={(date) => handleMonthUpdate(date.dateString)}
+        />
+      )}
+      {loading && (
+        <Center>
+          <View>
+            <Loading />
+          </View>
+        </Center>
+      )}
+    </View>
   );
 };
 
