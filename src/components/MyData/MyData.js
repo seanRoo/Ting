3d11e50cbc687dc96e87006f8f0a80b8b6023426
Sean Rooney <33811391+seanRoo@users.noreply.github.com>
2020-10-14 21:37:01 +0100

    hooking up real data and graphs!

--- src/components/MyData/MyData.js
+++ src/components/MyData/MyData.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, Dimensions } from 'react-native';
-import { Button, Segment, Card, CardItem } from 'native-base';
+import { Button, Segment, Card, CardItem, Left } from 'native-base';
 import LineChart from '../LineChart';
 import PieChart from '../PieChart';
 import GraphViewDropdown from './GraphViewDropdown';
@@ -8,65 +8,74 @@ import MyDataStyles from './MyData.styles';
 import auth from '@react-native-firebase/auth';
 import { DB } from '../../config';
 import Loading from '../Loading';
+import Entypo from 'react-native-vector-icons/Entypo';
+import { Right } from '../Right';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import {
+  monthsArray,
+  countOccurrences,
+  transformCountArray,
+} from './MyData.utils';
+import { Center } from '../Center';
 
 const MyData = () => {
   const currentUser = auth().currentUser.uid;
-  const [buttonState, setButtonState] = useState({
-    week: true,
-    month: false,
-    year: false,
+  const [graphView, setGraphView] = useState({
+    value: 'sounds',
+    text: 'Sounds',
   });
-  const [graphView, setGraphView] = useState('pie');
   const [countArray, setCountArray] = useState();
   const [pieData, setPieData] = useState();
   const [dataEntries, setDataEntries] = useState();
   const [loading, setLoading] = useState(true);
+  const [monthsArrayIndex, setMonthsArrayIndex] = useState(
+    new Date().getMonth(),
+  );
+  const [disabledArrows, setDisabledArrows] = useState({
+    rightArrow: false,
+    leftArrow: false,
+  });
+  const [showNoDataMessage, setShowNoDataMessage] = useState(false);
+  const [sliderData, setSliderData] = useState();
 
-  const transformCountArray = (countArray) => {
-    const transformedArray = [];
-    for (const [key, val] of Object.entries(countArray)) {
-      transformedArray.push({
-        name: key,
-        percentage: (Math.floor(val) / 15) * 100,
-        legendFontColor: 'black',
-        legendFontSize: 15,
-        color:
-          '#' +
-          (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6),
-      });
-    }
-    transformedArray.sort((a, b) => (a.name > b.name ? 1 : -1));
-    return transformedArray;
+  const handleMonthUpdate = (direction) => {
+    setLoading(true);
+    setShowNoDataMessage(false);
+    const newIndex = monthsArrayIndex + direction;
+    setMonthsArrayIndex(newIndex);
+    setDisabledArrows({
+      rightArrow: newIndex === 11,
+      leftArrow: newIndex === 0,
+    });
   };
 
-  const countOccurrences = (array) => {
-    const countArray = array.reduce((acc, curr) => {
-      if (typeof acc[curr] === 'undefined') {
-        acc[curr] = 1;
-      } else {
-        acc[curr] += 1;
-      }
-      return acc;
-    }, {});
-    return countArray;
-  };
-
-  const getMonthlyData = () => {
+  const getMonthlyData = (month) => {
     const today = new Date();
-    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const mm = month || String(today.getMonth() + 1).padStart(2, '0');
     const yyyy = String(today.getFullYear());
     const monthYearValue = `${yyyy}-${mm}`;
+    let test = null;
     try {
       DB.ref(`/checkIns/${currentUser}/${monthYearValue}`).on(
         'value',
         (querySnapshot) => {
           if (querySnapshot.val()) {
             const responseArray = Object.values(querySnapshot.val());
+            console.log(responseArray);
             setDataEntries(responseArray.length);
             const sounds = responseArray
               .map((element) => element.sounds)
               .flat();
+            console.log(sounds);
+            const sliderValues = responseArray
+              .map((element) => element.sliderValues)
+              .flat();
             setCountArray(countOccurrences(sounds));
+            setSliderData(sliderValues);
+            setLoading(false);
+          } else {
+            console.log('No data!');
+            setShowNoDataMessage(true);
             setLoading(false);
           }
         },
@@ -75,91 +84,114 @@ const MyData = () => {
       setLoading(false);
       console.log(error);
     }
+    return test;
   };
+
   useEffect(() => {
-    if (!countArray) {
-      getMonthlyData();
-    }
+    getMonthlyData(monthsArrayIndex + 1);
+  }, [monthsArrayIndex]);
+
+  useEffect(() => {
     if (countArray) {
       setPieData(transformCountArray(countArray));
     }
   }, [countArray]);
+
+  useEffect(() => {}, [sliderData]);
   return (
     <View style={MyDataStyles.container}>
-      <Segment style={MyDataStyles.buttonContainer}>
-        <Button
-          onPress={() =>
-            setButtonState({
-              week: true,
-              month: false,
-              year: false,
-            })
-          }
-          active={buttonState.week}
-          first
-          style={MyDataStyles.buttons}
+      <View style={{ width: '100%', flex: 1, flexDirection: 'column' }}>
+        <GraphViewDropdown
+          setGraphView={setGraphView}
+          graphView={graphView}
+          customStyles={{ marginLeft: 12 }}
+        />
+        <Card
+          style={{
+            width: Dimensions.get('window').width,
+            height: 300,
+          }}
         >
-          <Text>Week</Text>
-        </Button>
-        <Button
-          onPress={() =>
-            setButtonState({
-              week: false,
-              month: true,
-              year: false,
-            })
-          }
-          active={buttonState.month}
-          style={MyDataStyles.buttons}
-        >
-          <Text>Month</Text>
-        </Button>
-        <Button
-          onPress={() =>
-            setButtonState({
-              week: false,
-              month: false,
-              year: true,
-            })
-          }
-          active={buttonState.year}
-          last
-          style={MyDataStyles.buttons}
-        >
-          <Text>Year</Text>
-        </Button>
-      </Segment>
-      <View style={MyDataStyles.graphContainer}>
-        <View style={MyDataStyles.graphViewDropdown}>
-          <Text style={{ fontSize: 20 }}>Graph View</Text>
-          <GraphViewDropdown
-            setGraphView={setGraphView}
-            graphView={graphView}
-          />
-        </View>
-        <View style={{ alignSelf: 'center' }}>
-          {graphView === 'line' && <LineChart height={350} />}
-          {graphView === 'pie' && pieData && (
-            <Card
-              style={{
-                width: Dimensions.get('window').width - 20,
-                height: 300,
-              }}
-            >
-              {!loading && (
+          {!loading && (
+            <View>
+              <CardItem
+                header
+                style={{
+                  justifyContent: 'center',
+                  paddingBottom: 5,
+                }}
+              >
+                <Left>
+                  <TouchableOpacity
+                    disabled={disabledArrows.leftArrow}
+                    onPress={() => handleMonthUpdate(-1)}
+                  >
+                    <Entypo
+                      name="triangle-left"
+                      size={30}
+                      color={disabledArrows.leftArrow ? '#EBEBE4' : 'black'}
+                      style={{
+                        borderColor: 'black',
+                        borderWidth: 1,
+                        borderRadius: 12,
+                        padding: 5,
+                      }}
+                    />
+                  </TouchableOpacity>
+                </Left>
+                <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
+                  {monthsArray[monthsArrayIndex]}
+                </Text>
+                <Right>
+                  <TouchableOpacity
+                    disabled={disabledArrows.rightArrow}
+                    onPress={() => handleMonthUpdate(1)}
+                  >
+                    <Entypo
+                      name="triangle-right"
+                      size={30}
+                      color={disabledArrows.rightArrow ? '#EBEBE4' : 'black'}
+                      style={{
+                        borderColor: 'black',
+                        borderWidth: 1,
+                        borderRadius: 12,
+                        padding: 5,
+                      }}
+                    />
+                  </TouchableOpacity>
+                </Right>
+              </CardItem>
+              {!showNoDataMessage && (
                 <>
                   <CardItem>
-                    <PieChart data={pieData} />
+                    {graphView.value === 'sounds' && pieData && (
+                      <PieChart data={pieData} height={220} />
+                    )}
+                    {graphView.value === 'sleepAndStress' && sliderData && (
+                      <LineChart
+                        height={220}
+                        width={Dimensions.get('window').width - 30}
+                        data={sliderData}
+                      />
+                    )}
                   </CardItem>
                   <CardItem footer>
                     <Text>*Data from {dataEntries} entries</Text>
                   </CardItem>
                 </>
               )}
-              {loading && <Loading />}
-            </Card>
+              {showNoDataMessage && (
+                <CardItem style={{ height: 250 }}>
+                  <Center>
+                    <Text>No data found</Text>
+                    <Text>Have you checked in for this month?</Text>
+                  </Center>
+                </CardItem>
+              )}
+            </View>
           )}
-        </View>
+          {loading && <Loading />}
+        </Card>
       </View>
     </View>
   );
